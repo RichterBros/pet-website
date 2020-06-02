735690133738e088669b836c14d485aa6e7236b9
Erich Richter <erich.richter@gmail.com>
2020-06-02 11:53:03 -0700

    add background/text color change options > > co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+    $("#black").click(function() {
+        $("body").removeClass();
+        $("body").addClass("black-background");
+        $("p").removeClass();
+        $("p").addClass("white-text");
+        $("h2").removeClass();
+        $("h2").addClass("white-text");
 
+      });
+
+      $("#white").click(function() {
+        $("body").removeClass();
+        $("body").addClass("white-background");
+        $("p").removeClass();
+        $("p").addClass("black-text");
+        $("h2").removeClass();
+        $("h2").addClass("black-text");
+
+      });
 });
+
